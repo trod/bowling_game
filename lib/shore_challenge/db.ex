2cd6d40a54ac8eae389f27acc76bd524b8f1dc4b
boris.vainberg <boris.vainberg@kloeckner.com>
2021-03-02 12:19:45 +0100

    clean up

--- lib/shore_challenge/db.ex
+++ lib/shore_challenge/db.ex
@@ -1,4 +1,4 @@
-defmodule ShoreChallenge.Repo do
+defmodule ShoreChallenge.Db do
   use Ecto.Repo,
     otp_app: :shore_challenge,
     adapter: Ecto.Adapters.Postgres
--- mix.exs
+++ mix.exs
@@ -14,9 +14,6 @@ defmodule ShoreChallenge.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {ShoreChallenge.Application, []},
@@ -24,13 +21,9 @@ defmodule ShoreChallenge.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.4.7"},
@@ -43,17 +36,11 @@ defmodule ShoreChallenge.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create", "ecto.migrate", "test"]
     ]
   end
 end
